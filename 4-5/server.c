818308f083976d2d289dc5c9ccb31dbddb7bbe35
Rodion <99342659+Robi2810@users.noreply.github.com>
2023-06-11 23:43:23 +0300

    Update server.c

--- 4-5/server.c
+++ 4-5/server.c
@@ -27,7 +27,7 @@ int main(int argc, char *argv[])
     
     if (argc != 3)  
     {
-        fprintf(stderr, "Usage:  %s <Port for clients> <Total sum>\n", argv[0]);
+        fprintf(stderr, "Incorrect input:  %s <Port for clients> <Total sum>\n", argv[0]);
         exit(1);
     }
 
@@ -41,7 +41,7 @@ int main(int argc, char *argv[])
     server_addr.sin_port = htons(port);
 
     if (bind(server_socket, (struct sockaddr *) &server_addr, sizeof(server_addr)) < 0) DieWithError("bind() failed");
-    printf("Open socket on %s:%d\n", inet_ntoa(server_addr.sin_addr), port);
+    printf("Open %s:%d\n", inet_ntoa(server_addr.sin_addr), port);
     
     listen(server_socket, MAXPENDING);
     for (int i = 0; i < 8; i++) {
@@ -52,14 +52,14 @@ int main(int argc, char *argv[])
         recvfrom(server_socket, recvData, sizeof(recvData), 0, (struct sockaddr*) &client_addr, &client_length);
         childPart = recvData[0];
         sumFromClients += childPart;
-        printf("New connection from %s\n", inet_ntoa(client_addr.sin_addr));
+        printf("Connect from %s\n", inet_ntoa(client_addr.sin_addr));
     }
-    printf("Sum from children: %f\n", sumFromClients);
-    printf("Total sum: %f\n", totalSum);
+    printf("part of son: %f\n", sumFromClients);
+    printf("Summary: %f\n", totalSum);
     if (totalSum != sumFromClients) {
-        printf("Attorney is liar\n");
+        printf("Incorrect Lawyer-Skamer\n");
     } else {
-        printf("Everything is OK\n");
+        printf("CORRECT\n");
     }
     return 0;
 }
